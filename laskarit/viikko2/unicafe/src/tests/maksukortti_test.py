bb414bcf20b968abe1855fbfbdd7d695f8c00b7b
Tapir79 <saara.m.pakarinen@gmail.com>
2025-03-10 21:41:08 +0200

    Add exercise 6 tests

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,30 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+# Tehtävä 6: Takaisin testeihin
+# Kortin saldo alussa oikein
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+# Rahan lataaminen kasvattaa saldoa oikein
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(1000)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 20.0)
+
+# Rahan ottaminen toimii:
+## - Saldo vähenee oikein, jos rahaa on tarpeeksi
+    def test_saldo_vahenee_oikein_jos_rahaa_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 5.0)
+## - Saldo ei muutu, jos rahaa ei ole tarpeeksi
+    def test_saldo_ei_muutu_jos_rahaa_ei_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(1500)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+## - Metodi palauttaa True, jos rahat riittivät ja muuten False
+    def test_jos_raha_riittaa_palautuu_true(self):
+        palautus = self.maksukortti.ota_rahaa(900)
+        self.assertEqual(palautus, True)
+    def test_jos_raha_ei_riita_palautuu_false(self):
+        palautus = self.maksukortti.ota_rahaa(1500)
+        self.assertEqual(palautus, False)
