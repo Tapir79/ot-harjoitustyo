a3439a8c0b6e242ce7f4e6b65dc76305ba470d87
Tapir79 <saara.m.pakarinen@gmail.com>
2025-05-04 17:17:00 +0300

    Add docstring to create_sprite_info

--- src/utils/service_helpers.py
+++ src/utils/service_helpers.py
@@ -5,4 +5,16 @@ from models.sprite_info import SpriteInfo
 
 
 def create_sprite_info(point, size, speed, max_hits):
+    """
+    Create SpriteInfo with 0 hitcount.
+
+    Args:
+        point: (x,y)
+        size: (width, height)
+        speed: sprite speed
+        max_hits: maximum amount of hits the sprite can take.
+
+    Returns:
+        SpriteInfo object with full health.
+    """
     return SpriteInfo(point, size, speed, Hit(0, max_hits))
